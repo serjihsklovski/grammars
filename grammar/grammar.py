cb1ba40914f4f43840bd55bd191ed31e9f7322cd
serjihsklovski <serjihsklovski@gmail.com>
2017-02-26 23:07:30 +0300

    non-required parameter 'name'

--- grammar/grammar.py
+++ grammar/grammar.py
@@ -1,9 +1,10 @@
 class Grammar:
     def __init__(self, *, terminals, non_terminals, starting_non_terminal,
-                 transitions, restrictions):
+                 transitions, restrictions, **kwargs):
 
         self._terminals = terminals
         self._non_terminals = non_terminals
         self._starting_non_terminal = starting_non_terminal
         self._transitions = transitions
         self._restrictions = restrictions
+        self._name = kwargs.get('name', 'unknown')
