1525ee6c363742cc833312aaca3ba321f1601ceb
serjihsklovski <serjihsklovski@gmail.com>
2017-02-26 23:27:14 +0300

    'get_name', mocked 'generate_words' method

--- grammar/grammar.py
+++ grammar/grammar.py
@@ -8,3 +8,14 @@ class Grammar:
         self._transitions = transitions
         self._restrictions = restrictions
         self._name = kwargs.get('name', 'unknown')
+
+    def get_name(self):
+        return self._name
+
+    def generate_words(self):
+        words = set()
+        max_word_length = int(self._restrictions['max-word-length'])
+
+        # mocked loop
+        for i in range(10):
+            yield i
